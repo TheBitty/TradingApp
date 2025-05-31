83586dcec1e0d034fe1f6d54170dd9c46bdf4ece
bitty <hitsuu23@gmail.com>
2025-05-30 18:02:27 -0700

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,18 @@
-//master file ik we should use mutiple files but umm noo 
-
+# most of this code will relie on the struct to ensure the data is packed and unpacked in a consisitant format....(WHICH IS KEY)
+# as python has very limited ways to handle the memory opperations we do in our c++ backend!
+import struct 
+import time 
 from multiprocessing import shared_memory #for shared_memory
+import numpy as np # for math
 
-import numpy as np 
+def read_from_shared_memory(): # this function assumes the C++ backend has created the memory block....
+    try:
+        existing_shm = shm.SharedMemory(name = "/simplebuffer")
+        raw_memory = 
+    else:
+      print("C++ backend is not started or an error has occured in the creation of the memory block")
 
-shm = shared_memory.SharedMemory(create=True, size=a.nbytes)
-
-def GetSharedMemFromC(): #do we have to pass the memory address of our c++ mem?
-    print ("waiiting for file to open")
+    if _name_ == "_main_":
+        data = read_from_shared_memory()
+        if data = 1:
+            print("C++ BACKEND DATA:")
